3ef637c72f61d879d37e421bd37205b2e76afd58
Raymond <raymond96521>
2020-10-30 18:37:33 +0800

    Create a post form component

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+const BlogPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <View style={styles.view}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title="Save Blog Post" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    margin: 5,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+});
+
+export default BlogPostForm;
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,47 +1,18 @@
-import React, { useState, useContext } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
+
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  return (
-    <View style={styles.view}>
-      <Text>Enter New Title:</Text>
-      <TextInput
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-      <Text>Enter New Content:</Text>
-      <TextInput
-        value={content}
-        onChangeText={(newContent) => setContent(newContent)}
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
-const styles = StyleSheet.create({
-  view: {
-    margin: 5,
-  },
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
